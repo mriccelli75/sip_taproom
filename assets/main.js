263814b492686c2e3edfafbca03d8c063d7ac19a
Michael Riccelli <michaelriccelli@ons-fvff82l0q6lr.myfiosgateway.com>
2021-12-22 15:00:55 -0500

    added close button to alert message

--- assets/main.js
+++ assets/main.js
@@ -4,12 +4,12 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.padding = "0 0";
-    document.getElementById("business-alert").style.top = "60px";
   } else {
     document.getElementById("navbar").style.padding = ".5rem .5rem";
     // document.getElementById("brandLogo").style.fontSize = "35px";
-    document.getElementById("business-alert").style.top = "75px";
   }  
   
 }
+
+
 
